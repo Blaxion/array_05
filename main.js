02cf7a6ba9f5b2bb4b02c88a2b3e18e1c5c5c8d9
Blaxion <khibaziev.f@gmail.com>
2021-04-18 16:46:36 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,82 @@
+// 1
+let y;
+let x = ["", y , 0 , 0 , true ];
+console.log(x);
+// 2
+console.log(x.length);
+// 3
+x.pop();
+console.log(x);
+// 4
+x.shift();
+console.log(x);
+// 5
+x.push("Farhad","BGGGG");
+console.log(x);
+// 6
+x.unshift("r","t","x");
+console.log(x);
+// 7
+x.reverse();
+console.log(x);
+
+console.log("FIN");
+
+// 8
+// STAGE 1
+x=[];
+
+// STAGE 2
+x.push("Obj 1");
+
+// STAGE 3
+x.push("Obj 2");
+
+// STAGE 4
+alert(x);
+
+// STAGE 5
+x.unshift("Obj 3");
+
+// STAGE 6
+alert(x);
+
+// STAGE 7
+x.splice(4,0,"Obj 4","Obj 5","Obj 6");
+
+// STAGE 8
+alert(x);
+
+// STAGE 9
+x.splice(0,3)
+
+// STAGE 10
+x.push("objet 7");
+
+// STAGE 11
+alert(x.length);
+
+// STAGE 12
+x.splice((x.length)-2,2);
+
+// STAGE 13
+x.splice(0,2);
+
+// STAGE 14
+alert(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
